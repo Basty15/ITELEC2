0a9621791aab255605c4574784e81c8097abfa3d
Basty15 <2022312181@dhvsu.edu.ph>
2024-09-30 13:43:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
-    include_once 'config/setting-configuration.php';
-
+include_once 'config/setting-configuration.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,31 +7,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign In / Register</title>
-    <link rel="stylesheet" href="src/css/styles.css">
+    <link rel="stylesheet" href="src/style.css">
+    <title>Document</title>
 </head>
 <body>
-    <div class="container">
-        <div class="form-container">
-            <h1>SIGN IN</h1>
-            <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-                <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
-                <input type="email" name="email" placeholder="Enter Email" required><br>
-                <input type="password" name="password" placeholder="Enter Password" required><br>
-                <button type="submit" name="btn-signin">SIGN IN</button>
-            </form>
-        </div>
-
-        <div class="form-container">
-            <h1>REGISTER</h1>
-            <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-                <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
-                <input type="text" name="username" placeholder="Enter Username" required><br>
-                <input type="email" name="email" placeholder="Enter Email" required><br>
-                <input type="password" name="password" placeholder="Enter Password" required><br>
-                <button type="submit" name="btn-signup">SIGN UP</button>
-            </form>
-        </div>
-    </div>
+    <h1>SIGN IN</h1>
+    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
+        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
+        <input type="email" name="email" placeholder="Enter email" required> <br>
+        <input type="password" name="password" placeholder="Enter password" required> <br>
+        <button type="submit" name="btn-signin">SIGN IN</button>
+    </form>
+   
+   
+    <h1>REGISTRAION</h1>
+    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
+        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
+        <input type="text" name="username" placeholder="Enter username" required> <br>
+        <input type="email" name="email" placeholder="Enter email" required> <br>
+        <input type="password" name="password" placeholder="Enter password" required> <br>
+        <button type="submit" name="btn-signup">SIGN UP</button>
+    </form>
 </body>
 </html>
